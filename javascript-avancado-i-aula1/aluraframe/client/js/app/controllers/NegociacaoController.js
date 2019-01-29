015e6e529049acf097f6e02e94bf1d584d3dbba4
Júnior <junior.jaava@gmail.com>
2019-01-29 21:10:06 +0000

    Adding date helper and isolating the date logic

--- javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -5,26 +5,23 @@ class NegociacaoController {
         let $ = document.querySelector.bind(document);
 
         this._inputData = $('#data');
-        this._inputQuantidade =  $('#quantidade');
+        this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-    } 
-
-  adiciona(event) {
-
-    event.preventDefault();
-
-    let data = new Date(...
-        this._inputData.value
-        .split('-')
-        .map((item, indice) => item - indice % 2)
-    );
-    
-    let negociacao = new Negociacao(
-        data,
-        this._inputQuantidade.value,
-        this._inputValor.value
-      );
-    
-      console.log(negociacao);
     }
+
+    adiciona(event) {
+
+        event.preventDefault();
+
+        let helper = new DateHelper();
+
+        let negociacao = new Negociacao(
+            helper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+
+        //console.log(negociacao);
+    }
+
 }
