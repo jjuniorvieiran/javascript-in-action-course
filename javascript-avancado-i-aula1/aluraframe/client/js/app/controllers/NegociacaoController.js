b3b3f6b7462885639cc763f55a9a02b8a6fc5dda
Júnior <junior.jaava@gmail.com>
2019-02-02 15:56:36 +0000

    Working with view; Using innerhtml;

--- javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -8,12 +8,18 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
         this._listaNegociacoes = new ListaNegociacoes();
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+        this._negociacoesView.update();
+
     }
 
     adiciona(event) {
 
         event.preventDefault();
+    
         this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._listaNegociacoes.negociacoes.push(this._criaNegociacao());
+
         this._limpaFormulario();
     }
 
