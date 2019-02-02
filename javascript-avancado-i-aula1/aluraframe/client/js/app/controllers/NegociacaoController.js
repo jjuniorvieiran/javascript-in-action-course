a0478bd0f0c87fde3486a79f9347b1bb9912bb93
Júnior <junior.jaava@gmail.com>
2019-02-02 16:24:19 +0000

    Working with map function; rendering dynamically items in the table

--- javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -8,8 +8,9 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
         this._listaNegociacoes = new ListaNegociacoes();
+
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._negociacoesView.update();
+        this._negociacoesView.update(this._listaNegociacoes);
 
     }
 
@@ -18,12 +19,12 @@ class NegociacaoController {
         event.preventDefault();
     
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._listaNegociacoes.negociacoes.push(this._criaNegociacao());
-
+        this._negociacoesView.update(this._listaNegociacoes);
         this._limpaFormulario();
     }
 
     _criaNegociacao() {
+        
         return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
