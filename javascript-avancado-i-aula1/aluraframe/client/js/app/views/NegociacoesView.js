6be58ffb4708b9fc655735b16d16c51419d0e66a
Júnior <junior.jaava@gmail.com>
2019-02-06 20:49:10 +0000

    Working with Inheritance; Using contructor

--- javascript-avancado-i-aula1/aluraframe/client/js/app/views/NegociacoesView.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/views/NegociacoesView.js
@@ -1,8 +1,7 @@
-class NegociacoesView {
+class NegociacoesView extends View{
 
     constructor(elemento) {
-
-        this._elemento = elemento;
+        super(elemento);
     }
 
     _template(model) {
@@ -37,10 +36,5 @@ class NegociacoesView {
             </tfoot>
         </table>
             `;
-    }
-
-    update(model) {
-
-        this._elemento.innerHTML = this._template(model);
     }
 }
